6da8c7e2c3ec32db21f4105831e908fde3f4c948
Luz-Noh24 <luznoh274@gmail.com>
2023-12-04 18:44:13 -0600

    pr

--- Proyecto-gif-Luz-Noh/src/ProyectoGif.jsx
+++ Proyecto-gif-Luz-Noh/src/ProyectoGif.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { AddCategory, GifGrid } from "./componentes";
+import { AddCategory, GifGrid } from "./components";
 export const ProyectoGif = () => {
   
   
